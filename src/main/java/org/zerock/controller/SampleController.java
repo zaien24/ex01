0e84804e87d446a781822dec7e48b0d411f0f6d1
zaien24 <zaien24@gmail.com>
2020-08-27 22:48:35 +0900

    [pom.xml]  # 자바 버전 변경  # 스프링 버전 변경  # log4j 변경  # 서블릿 버전 변경  # junit 변경  # spring-test 변경  # 롬북 추가  # 자바 버전 변경  # 메이븐 컴파일러 버전 변경
    [RequestMapping]
    
    [GetMapping]
    
    [SampleDTO]

---
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,40 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.domain.SampleDTO;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/sample/*")
+@Log4j
+public class SampleController {
+	
+	@RequestMapping("")
+	public void basic() {
+		
+		log.info("basic..................");
+
+	}
+	
+	@RequestMapping(value = "/basic", method = {RequestMethod.GET, RequestMethod.POST})
+	public void basicGet() {
+		log.info("basic get.....................");
+	}
+	
+	@GetMapping("/basicOnlyGet")
+	public void basicGet2() {
+		log.info("basic get only get...............");
+	}
+	
+	@GetMapping("/ex01")
+	public String ex01(SampleDTO dto) {
+		log.info("" + dto);
+		
+		return "ex01";
+	}
+	
+}
