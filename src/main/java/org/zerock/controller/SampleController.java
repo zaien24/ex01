1a7fa2a9ed64d8615029357d08fb088cb9f794ce
zaien24 <zaien24@gmail.com>
2020-08-27 23:15:34 +0900

    [파라미터 수집]  # 기본형  # 리스트  # 배열  # 객체 리스트
    [@InitBinder]

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,10 +1,20 @@
 package org.zerock.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -35,6 +45,58 @@ public class SampleController {
 		log.info("" + dto);
 		
 		return "ex01";
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(@RequestParam("name") String name, 
+					   @RequestParam("age") int age) {
+		
+		log.info("name : " + name);
+		log.info("age : " + age);
+		
+		return "ex02";
+		
+	}
+	
+	@GetMapping("/ex02List")
+	public String ex02List(@RequestParam("ids") ArrayList<String> ids) {
+		
+		log.info("ids : " + ids);
+		
+		return "ex02List";
+		
+	}
+	
+	@GetMapping("/ex02Array")
+	public String ex02Array(@RequestParam("ids") String[] ids) {
+		
+		log.info("array ids : " + Arrays.toString(ids));
+		
+		return "ex02List";
+		
+	}
+	
+	@GetMapping("/ex02Bean")
+	public String ex02Bean(SampleDTOList list) {
+		
+		log.info("list dtos: " + list);
+		
+		return "ex02Bean";
+		
+	}
+	
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+		
+	}
+	
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo : " + todo);
+		return "ex03";
 	}
 	
 }
---
+++ src/main/java/org/zerock/domain/TodoDTO.java
@@ -0,0 +1,14 @@
+package org.zerock.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class TodoDTO {
+	
+	private String title;
+	private Date dueDate;
+	
+
+}
